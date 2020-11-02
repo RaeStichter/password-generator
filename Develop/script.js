4aa3b676419e2825d494553c2d26caee1ba9bcc3
Rae Stichter <raestichter@gmail.com>
2020-11-01 23:35:57 -0500

    cleaned up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,15 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// ---------------------------------- FUNCTION CALLED ON CLICK -----------------------------------------
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-  console.log("Function reached");
-  getPasswordLength();
-  promptSelect();
-  console.log(passwordSelections);
-  availableCharacters();
-  generatePassword();
-  document.getElementById("password").innerHTML = password.value;
+  // THIS FUNCTION CALLS ALL OTHER FUNCTIONS
+  getPasswordLength(); // get length of password desired from user
+  promptSelect(); // user selected promts and verification that at least on is selected
+  availableCharacters(); // based on selected promts, which characters will be available
+  generatePassword(); // generate a random password given the length of password and the available characters
+  document.getElementById("password").innerHTML = password.value; // display back to the HTML page
 }
 
 // --------------------------------- FUNCTIONS TO GET INFO FROM PROMPTS-------------------------------------
@@ -40,13 +33,13 @@ var getPasswordLength = function() {
     }
     // if the integer is not between 8 and 128, return prompt to try again
     else {
-      window.alert("please enter a number between 8 and 128");
+      window.alert("Please enter a number between 8 and 128.");
       return getPasswordLength();
     }
   }
   // if integer was not entered, return prompt to try again
   else {
-    window.alert("please enter a number");
+    window.alert("Please enter a number.");
     return getPasswordLength();
   }
 }
@@ -65,12 +58,12 @@ var promptSelect = function() {
       var confirmLowercase = window.confirm("Would you like lowercase letters in your password?");
       // if yes (true), set to true
       if (confirmLowercase) {
-        window.alert("your password will include lowercase letters.");
+        window.alert("Your password WILL include lowercase letters.");
         passwordSelections.passwordLowercase = true;
       }
       // if cancel (false), set to false
       else {
-        window.alert("your password will not include lowercase letters.");
+        window.alert("Your password WILL NOT include lowercase letters.");
         passwordSelections.passwordLowercase = false;
       }
 
@@ -79,12 +72,12 @@ var promptSelect = function() {
       var confirmUppercase = window.confirm("Would you like uppercase letters in your password?");
       // if yes (true), set to true
       if (confirmUppercase) {
-        window.alert("your password will include uppercase letters.");
+        window.alert("Your password WILL include uppercase letters.");
         passwordSelections.passwordUppercase = true;
       }
       // if cancel (false), set to false
       else {
-        window.alert("your password will not include uppercase letters.");
+        window.alert("Your password WILL NOT include uppercase letters.");
         passwordSelections.passwordUppercase = false;
       }
 
@@ -93,12 +86,12 @@ var promptSelect = function() {
       var confirmNumeric = window.confirm("Would you like numbers in your password?");
       // if yes (true), set to true
       if (confirmNumeric) {
-        window.alert("your password will include numbers.");
+        window.alert("Your password WILL include numbers.");
         passwordSelections.passwordNumeric = true;
       }
       // if cancel (false), set to false
       else {
-        window.alert("your password will not include numbers.");
+        window.alert("Your password WILL NOT include numbers.");
         passwordSelections.passwordNumeric = false;
       }
 
@@ -107,12 +100,12 @@ var promptSelect = function() {
       var confirmSpecial = window.confirm("Would you like special characters in your password?");
       // if yes (true), set to true
       if (confirmSpecial) {
-        window.alert("your password will include special characters.");
+        window.alert("Your password WILL include special characters.");
         passwordSelections.passwordSpecial = true;
       }
       // if cancel (false), set to false
       else {
-        window.alert("your password will not include special characters.");
+        window.alert("Your password WILL NOT include special characters.");
         passwordSelections.passwordSpecial = false;
       }
   }
@@ -125,7 +118,7 @@ var promptSelect = function() {
     passwordSelections.passwordSpecial === false
   ) {
     // if all responses were declined
-    window.alert("you have not chosen any character types! Please return and  select at least one character type.");
+    window.alert("You have not chosen any character types! Please return and select at least one character type.");
     // reset saved value so the questions will cycle
     passwordSelections.reset();
     // restart this function
@@ -138,22 +131,26 @@ var promptSelect = function() {
 }
 
 // --------------------------------- AVAILABLE CHARACTERS ----------------------------------------------------
+// based on the user's selections, this function will determine which characters are applicable.
 var availableCharacters = function() {
+  // if lowercase values are wanted, add them to availChar.  If not, do nothing.
   if (passwordSelections.passwordLowercase === true) {
     availChar = passwordText.lowercase;
   }
+  // if uppercase values are wanted, add them to availChar.  If not, do nothing.
   if (passwordSelections.passwordUppercase === true) {
     availChar = availChar + passwordText.uppercase;
   }
+  // if numeric values are wanted, add them to availChar.  If not, do nothing.
   if (passwordSelections.passwordNumeric === true) {
     availChar = availChar + passwordText.number;
   }
+  // if special values are wanted, add them to availChar.  If not, do nothing.
   if (passwordSelections.passwordSpecial === true) {
     availChar = availChar + passwordText.special;
   }
   console.log(availChar);
   return availChar;
-
 }
 
 // --------------------------------- PASSWORD GENERATION ----------------------------------------------------
@@ -161,21 +158,22 @@ var generatePassword = function () {
     // for loop which will take the availChar string and randomize a password based on the length of password desired
     var pass = "";
   for (i = 0; i < passwordSelections.passwordLength; i++) {
-    //debugger;
+    // based on the length of the available characters, find whole random number.
     var characters = Math.floor(Math.random() * availChar.length + 1);
 
+    // each iteration through the loop add the additional character until the desired password length is reached.
     pass += availChar.charAt(characters);
 
   }
+  // Set the values of the final password object. These will be passed to HTML
   password.value = pass;
   password.length = passwordSelections.passwordLength;
-  //console.log(pass);
   console.log("password value: " + password.value + ". Password length: " + password.length);
   }
 
 // ---------------------------------- OBJECTS AND VARIABLES -------------------------------------------------
 // passwordSelections is an object which will hold all of the answers to the prompts
-// each element will call another function to generate this information
+// each element will be set in the above functions
 var passwordSelections = {
   passwordLength: "",
   passwordLowercase: "", // getLowercase:
@@ -190,6 +188,7 @@ var passwordSelections = {
   }
 }
 
+// These are the possible characters for the random password generation.
 var passwordText = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -197,10 +196,12 @@ var passwordText = {
   special: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 }
 
+// object to hold the final password information.
 var Password = {
   value: "",
   length: ''
 }
 
+// ----------------------------------------- EVENT LISTENER --------------------------------
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
